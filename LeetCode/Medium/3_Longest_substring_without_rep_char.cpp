b3a0f9b3e6f98001b3ec3bdac6cf908243bb1800
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-02-06 22:35:09 +0530

    Oldq

---
+++ LeetCode/Medium/3_Longest_substring_without_rep_char.cpp
@@ -0,0 +1,23 @@
+#include <set>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> st;
+        int l = 0, maxLen = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            while (st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
+            }
+
+            st.insert(s[r]);
+            maxLen = max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+};
