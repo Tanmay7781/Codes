418ae80d2e724d8c1b1734eb02746232e917dd7a
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-02-09 23:59:08 +0530

    new

---
+++ LeetCode/Medium/33_Search_in_rotated_sorted_array.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int modified_bs(vector<int>& nums, int target,int l,int h)
+    {
+        
+        while(l<=h)
+        {
+            int mid = (l+h)/2;
+
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+
+            else if(nums[l]<=nums[mid])
+            {
+                if(nums[l]<=target && target<=nums[mid])
+                {
+                    h=mid-1;
+                }
+
+                else
+                {
+                    l=mid+1;
+                }
+            }
+
+            else
+            {
+                if(nums[mid]<target && target<=nums[h])
+                {
+                    l=mid+1;
+                }
+
+                else
+                {
+                    h=mid-1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        int l=0,h=nums.size()-1;
+
+        return modified_bs(nums,target,l,h);
+        
+
+    }
+};
