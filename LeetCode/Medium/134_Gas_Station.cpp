9b43518f4fb7fae494215128e38526b23f9a72f2
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-02-12 23:50:50 +0530

    120226

---
+++ LeetCode/Medium/134_Gas_Station.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalgas = 0, totalcost = 0;
+        int tank = 0, start = 0;
+
+        for (int i = 0; i < gas.size(); i++) {
+            totalgas += gas[i];
+            totalcost += cost[i];
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return (totalgas >= totalcost ? start : -1);
+    }
+};
