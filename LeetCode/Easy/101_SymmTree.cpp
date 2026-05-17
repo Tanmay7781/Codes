ac6382306098b2051d5479e18ae9d115e66b9c7e
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-05-17 12:18:52 +0530

    SymmTree

---
+++ LeetCode/Easy/101_SymmTree.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool helper(TreeNode* right, TreeNode* left) {
+        if (left == NULL || right == NULL) {
+            return (left == right);
+        }
+
+        if (left->val != right->val)
+            return false;
+
+        return helper(left->left, right->right) &&
+               helper(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return root == NULL || helper(root->left, root->right);
+    }
+};
