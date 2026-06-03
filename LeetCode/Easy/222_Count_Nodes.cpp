220e72694d1a2282644332062666ce7d91ad212c
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-06-03 14:39:01 +0530

    CountNodes

---
+++ LeetCode/Easy/222_Count_Nodes.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void countNodes(TreeNode* root, int& count) {
+        if (root == NULL)
+            return;
+
+        count++;
+        countNodes(root->left, count);
+        countNodes(root->right, count);
+    }
+
+    int countNodes(TreeNode* root) {
+        int count = 0;
+        countNodes(root, count);
+        return count;
+    }
+};
