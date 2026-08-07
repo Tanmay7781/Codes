967ca607ac03dd31976507ac2a60d8cdd3200e7d
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-08-07 23:00:10 +0530

    HouseRobberDP03Memoization

---
+++ LeetCode/Medium/198_House_Robber_I_DP03.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+using namespace std;
+
+//memoization-recursive
+class Solution {
+public:
+    int maxSum(int idx, vector<int>& nums, vector<int>& dp) {
+        if (idx == 0)
+            return nums[idx];
+        if (idx < 0)
+            return 0;
+
+        if (dp[idx] != -1)
+            return dp[idx];
+
+        int pick = nums[idx] + maxSum(idx - 2, nums, dp);
+        int notPick = 0 + maxSum(idx - 1, nums, dp);
+
+        return dp[idx] = max(pick, notPick);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+
+        return maxSum(n - 1, nums, dp);
+    }
+};
+
+//tabulation
