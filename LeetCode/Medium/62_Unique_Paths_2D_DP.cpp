d063f8e40a60c90a06ef7f372d4c2e54359d042f
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-08-11 01:16:22 +0530

    UniquePathMemoization

---
+++ LeetCode/Medium/62_Unique_Paths_2D_DP.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+// memoization
+class Solution
+{
+public:
+    int f(int r, int c, int m, int n, vector<vector<int>> &dp)
+    {
+        if (r == m - 1 && c == n - 1)
+            return 1;
+        if (r >= m || c >= n)
+            return 0;
+        if (dp[r][c] != -1)
+            return dp[r][c];
+
+        int right = f(r, c + 1, m, n, dp);
+        int left = f(r + 1, c, m, n, dp);
+
+        return dp[r][c] = right + left;
+    }
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(0, 0, m, n, dp);
+    }
+};
