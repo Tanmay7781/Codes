d1fdef0fa32b792b2c063aecaeda0204840d70bd
Tanmay7781 <tanmayofficial2511@gmail.com>
2026-08-24 10:19:55 +0530

    CoinChangeIIMemoizationTab

--- LeetCode/Medium/518_Coin_Change_II.cpp
+++ LeetCode/Medium/518_Coin_Change_II.cpp
@@ -35,3 +35,46 @@ public:
         return (ans) ? ans : 0;
     }
 };
+
+// tabulation
+class Solution
+{
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        int n = coins.size();
+        vector<vector<long long>> dp(n, vector<long long>(amount + 1, 0));
+
+        for (int ind = 0; ind < n; ind++)
+        {
+            dp[ind][0] = 1;
+        }
+
+        for (int T = 0; T <= amount; T++)
+        {
+            dp[0][T] = (T % coins[0] == 0) ? 1 : 0;
+        }
+
+        for (int ind = 1; ind < n; ind++)
+        {
+            for (int T = 1; T <= amount; T++)
+            {
+                long long notPick = dp[ind - 1][T];
+                long long pick = 0;
+                if (coins[ind] <= T)
+                    pick = dp[ind][T - coins[ind]];
+
+                if (pick > INT_MAX - notPick)
+                {
+                    dp[ind][T] = INT_MAX;
+                }
+                else
+                {
+                    dp[ind][T] = pick + notPick;
+                }
+            }
+        }
+
+        return dp[n - 1][amount];
+    }
+};
